081298f6b14239c8c523b2c357812e740b2f3b99
EC2 Default User <ec2-user@ip-172-31-36-207.ap-south-1.compute.internal>
2024-05-08 12:45:47 +0000

    recent changes

--- shoelace.sh
+++ shoelace.sh
@@ -3,7 +3,8 @@ sleep 10s
 sudo yum update; sudo yum install ansible -y
 sleep 10s
 export ANSIBLE_HOST_KEY_CHECKING=False
-git clone https://github.com/sharan0-0/Tuesday.git
+cd ~
+git clone https://github.com/sharan0-0/Saturday.git
 sleep 10s
 ansible-playbook /Tuesday/tf_ans/Ansible/MediaWiki/release.yml
 sleep 60s
